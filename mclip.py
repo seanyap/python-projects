ec55480255ead24d0ee7a3c2c2eaada14e45fcb3
Sean <syap0317@gmail.com>
2021-01-03 19:49:56 -0500

    created the multi-clipboard program

---
+++ mclip.py
@@ -0,0 +1,23 @@
+#! python3
+# mclip.py - A multi-clipboard program
+
+TEXT = {
+    'agree': """Yes, I agree. That sounds fine to me.""",
+    'busy': """Sorry, can we do this later this week or next week?""",
+    'upsell': """Would you consider making this a monthly donation?"""
+}
+
+# command line arguments are stored in the variable - sys.argv
+# 1st arg: program's filename   2nd arg: first command line arg
+import sys, pyperclip
+if len(sys.argv) < 2:
+    print('Usage: python mclip.py [keyphrase] - copy phrase text')
+    sys.exit()
+
+keyphrase = sys.argv[1]
+
+if keyphrase in TEXT: 
+    pyperclip.copy(TEXT[keyphrase])
+    print('Text for ' + keyphrase + ' copied to clipboard.')
+else:
+    print('There is no text for ' + keyphrase)
